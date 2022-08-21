51d8bcca072bd5224a2eced4e87d3a190667f4cb
qwertydiy2 <107488390+qwertydiy2@users.noreply.github.com>
2022-08-21 14:45:48 +0100

    Add files via upload

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+let assert = chai.assert;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+let convertHandler = new ConvertHandler();
+
+suite('Unit Tests', function(){
+  assert.equal(convertHandler.getNum("4gal"),'4')
+
+});
